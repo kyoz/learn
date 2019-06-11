db03890e7c870c01c0370bbef9840584776fc5b1
Kyoz <banminkyoz@gmail.com>
2019-06-11 20:16:56 +0700

    update

--- tools/webpack/basic/src/index.js
+++ tools/webpack/basic/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack', 'from script'], ' ');
+  element.classList.add('hello');
+
+  // Add image to existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  // Log xml data
+  console.log(Data);
 
   return element;
 }
