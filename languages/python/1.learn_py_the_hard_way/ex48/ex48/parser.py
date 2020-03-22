4b703dbf6ccd958655c404fe49a225f4288dee60
Kyoz <banminkyoz@gmail.com>
2020-03-22 23:17:40 +0700

    learn(python)

--- languages/python/1.learn_py_the_hard_way/ex48/ex48/parser.py
+++ languages/python/1.learn_py_the_hard_way/ex48/ex48/parser.py
@@ -55,7 +55,7 @@ def parse_subject(word_list):
     skip(word_list, 'stop')
     next_word = peek(word_list)
 
-    if next_word == peek(word_list):
+    if next_word == 'noun':
         return match(word_list, 'noun')
     elif next_word == 'verb':
         return ('noun', 'player')
